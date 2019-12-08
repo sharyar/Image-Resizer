1fb2559e057f18c52646dd7375c82c2d552d631e
Sharyar Memon <sharyar@ualberta.ca>
2019-12-07 23:26:19 -0700

    V 0.1 - Non Functional. Only contains UI elements. Does not have resizer function built in yet.

---
+++ resizer.py
@@ -0,0 +1,77 @@
+# Author: Sharyar Memon
+# Project: Multi-Image Resizer in Python with a GUI
+# Date Started: December 7, 2019
+# Editor: Atom
+
+# Import required modules
+from tkinter import *
+from tkinter import filedialog
+from tkinter import ttk
+from PIL import Image
+import glob, os
+
+# Define variables that will be used throught the program
+
+sizes = list()
+source_folder = ''
+dest_folder = ''
+
+
+
+# Main function that resizes Images
+def resizer_fun(sizes, images):
+    pass
+
+
+# Folder Selection Functions for source and destination
+
+def select_source():
+    global source_folder
+    source_folder = filedialog.askdirectory(initialdir = '/', title = 'Please select the directory you would like to use as the source of your images')
+
+def select_dest():
+    global dest_folder
+    dest_folder = filedialog.askdirectory(initialdir = '/', title = 'Please select the directory you would like your files to be save to')
+
+# Get Size function for get size button. It takes the Stringvar (size_var) and converts it to a list of integers. Its assigned to a button once the user has input their info.
+
+def get_sizes(size_var):
+    global sizes
+    image_size_str = str(size_var.get())
+    sizes = [int(x) for x in image_size_str.strip(' ').split(',')]
+    print (sizes)
+# Gui for user input
+root = Tk()
+
+size_var = StringVar()
+
+# Heading
+heading1 = ttk.Label(text = 'Image Resizer Program')
+heading1.pack()
+
+
+# Buttons for source & dest folders
+
+but_source = ttk.Button(root, text = 'Source Folder', command = select_source)
+but_source.pack()
+
+but_dest = ttk.Button(root, text = 'Destination Folder', command = select_dest)
+but_dest.pack()
+
+size_entry = Entry(root, textvariable = size_var)
+size_entry.pack()
+
+but_get_sizes = ttk.Button(root, text = 'Store Sizes', command = lambda: get_sizes(size_var))
+but_get_sizes.pack()
+# panedwindow = ttk.Panedwindow(root, orient = HORIZONTAL)
+# panedwindow.pack(fill = BOTH, expand = True)
+# frame1 = ttk.Frame(panedwindow, width = 100, height=300, relief = SUNKEN)
+# frame2 = ttk.Frame(panedwindow, width = 100, height=300, relief = SUNKEN)
+#
+# panedwindow.add(frame1, weight = 1)
+# panedwindow.add(frame2, weight = 2)
+
+
+
+# initialize Gui
+root.mainloop()
